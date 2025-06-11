b05c4582262d50c85d8388d83b1d55ef923c867f
xyf2002 <2316639449@qq.com>
2025-06-11 13:55:49 +0100

    update file name

--- scripts/install_custom_kernel.sh
+++ scripts/install_custom_kernel.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-
 VERSION="5.15.160+"
 ARCHIVE="chronos-kernel-backup-${VERSION}.tar.gz"
 RELEASE_URL="https://github.com/xyf2002/compute-server/releases/download/image"
@@ -20,12 +19,14 @@ wget -P "${DOWNLOAD_DIR}" "${RELEASE_URL}/${ARCHIVE}"
 echo "[+] Extracting..."
 tar -xzf "${DOWNLOAD_DIR}/${ARCHIVE}" -C "${DOWNLOAD_DIR}"
 
+KERNEL_DIR="${DOWNLOAD_DIR}/chronos-kernel-backup"
+
 echo "[+] Copying kernel files to /boot and /lib/modules..."
-sudo cp "${DOWNLOAD_DIR}/chronos-kernel-backup-${VERSION}/vmlinuz-${VERSION}" /boot/
-sudo cp "${DOWNLOAD_DIR}/chronos-kernel-backup-${VERSION}/initrd.img-${VERSION}" /boot/
-sudo cp "${DOWNLOAD_DIR}/chronos-kernel-backup-${VERSION}/System.map-${VERSION}" /boot/
-sudo cp "${DOWNLOAD_DIR}/chronos-kernel-backup-${VERSION}/config-${VERSION}" /boot/
-sudo cp -r "${DOWNLOAD_DIR}/chronos-kernel-backup-${VERSION}/modules/${VERSION}" /lib/modules/
+sudo cp "${KERNEL_DIR}/vmlinuz-${VERSION}" /boot/
+sudo cp "${KERNEL_DIR}/initrd.img-${VERSION}" /boot/
+sudo cp "${KERNEL_DIR}/System.map-${VERSION}" /boot/
+sudo cp "${KERNEL_DIR}/config-${VERSION}" /boot/
+sudo cp -r "${KERNEL_DIR}/modules/${VERSION}" /lib/modules/
 
 echo "[+] Updating GRUB..."
 sudo update-grub
