4098d91b9d68ffa2e3c2d42dca529e60ffa47efd
xyf2002 <2316639449@qq.com>
2025-06-12 10:31:53 +0100

    update profile

--- profile.py
+++ profile.py
@@ -245,7 +245,7 @@ for i in range(0,params.machineNum):
     node.addService(PG.Execute(shell="bash", command=profileConfigs + "/local/repository/scripts/configure.sh"))
 
     # node.addService(PG.Execute(shell="bash", command="/local/repository/scripts/install_custom_kernel.sh"))
-    command = f"/local/repository/scripts/build_kernel.sh {params.token}"
+    command="/local/repository/scripts/build_kernel.sh {}".format(params.token)
     node.addService(PG.Execute(shell="bash", command=command))
     node.hardware_type = params.Hardware
     iface = node.addInterface()
