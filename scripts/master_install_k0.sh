3ed8b678996cb3c89301785ace6e050b69331c3b
Ujjwal Pawar <s2325287@ed.ac.uk>
2025-06-26 15:24:31 +0100

    change order in master_install_k0.sh

--- scripts/master_install_k0.sh
+++ scripts/master_install_k0.sh
@@ -36,10 +36,9 @@ sudo cp /var/lib/k0s/pki/admin.conf ~/admin.conf
 echo 'export KUBECONFIG=~/admin.conf' >> ~/.bashrc
 sudo chown ubuntu ~/admin.conf
 chmod g-r ~/admin.conf
-kubectl taint nodes ins0vm node-role.kubernetes.io/control-plane-
 sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
 echo -e '#!/bin/bash\nexec k0s kubectl "$@"' | sudo tee /usr/local/bin/kubectl > /dev/null
 sudo chmod +x /usr/local/bin/kubectl
-
+kubectl taint nodes ins0vm node-role.kubernetes.io/control-plane-
 #Generate and save Worker token
 log "Worker join-token written to /home/ubuntu/token-file"
